aec17071c759bb836b74c3341c30d958e2528542
Anatolii-med <67787169+Anatolii-med@users.noreply.github.com>
2021-06-04 15:52:17 +0300

    task 1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,17 @@
 const NavEl = document.querySelectorAll("li.item");
 console.log(`В списке ${NavEl.length} категории.`);
 
-const TitleEl = document.querySelectorAll("h2");
-console.log(TitleEl);
+const animals = NavEl[0].firstElementChild.textContent;
+const countAnimals = NavEl[0].lastElementChild.children.length;
+console.log(`Категория: ${animals}`);
+console.log(`Количество элементов: ${countAnimals}`);
+
+const prodcts = NavEl[1].firstElementChild.textContent;
+const countProdcts = NavEl[1].lastElementChild.children.length;
+console.log(`Категория: ${prodcts}`);
+console.log(`Количество элементов: ${countProdcts}`);
+
+const technologys = NavEl[2].firstElementChild.textContent;
+const countTechnologys = NavEl[2].lastElementChild.children.length;
+console.log(`Категория: ${technologys}`);
+console.log(`Количество элементов: ${countTechnologys}`);
