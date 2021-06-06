580617ab36f2b34e9745fe407504d49c55ac3899
Anatolii <anatolii@github.com>
2021-06-06 22:27:43 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,33 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+
+const potato = document.createElement('li');
+potato.textContent = ingredients[0];
+
+
+const mushroom = document.createElement('li');
+mushroom.textContent = ingredients[1];
+
+
+const garlic = document.createElement('li');
+garlic.textContent = ingredients[2];
+
+
+const tomato = document.createElement('li');
+tomato.textContent = ingredients[3];
+
+
+const greens = document.createElement('li');
+greens.textContent = ingredients[4];
+
+
+const condiments = document.createElement('li');
+condiments.textContent = ingredients[5];
+
+
+const ingredientsList = document.querySelector("#ingredients");
+
+
+ingredientsList.append(potato, mushroom, garlic, tomato, greens, condiments);
