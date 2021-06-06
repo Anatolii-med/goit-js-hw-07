76260abfa43787798b416b27726e19f00b0d5a49
Anatolii <anatolii@github.com>
2021-06-06 23:43:32 +0300

    task 3 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,25 @@
-const NavEl = document.querySelectorAll("li.item");
-console.log(`В списке ${NavEl.length} категории.`);
+const NavEl = document.querySelector("#categories");
+const itemsEl = NavEl.children;
+console.log(`В списке ${itemsEl.length} категории.`);
 
-const animals = NavEl[0].firstElementChild.textContent;
-const countAnimals = NavEl[0].lastElementChild.children.length;
-console.log(`Категория: ${animals}`);
-console.log(`Количество элементов: ${countAnimals}`);
+for (const item of itemsEl) {
+  const categoryEl = item.firstElementChild.textContent;
+  const elCount = item.lastElementChild.children.length;
+  console.log(`Категория: ${categoryEl}`);
+  console.log(`Количество элементов: ${elCount}`);
+}
 
-const prodcts = NavEl[1].firstElementChild.textContent;
-const countProdcts = NavEl[1].lastElementChild.children.length;
-console.log(`Категория: ${prodcts}`);
-console.log(`Количество элементов: ${countProdcts}`);
+// const animals = NavEl[0].firstElementChild.textContent;
+// const countAnimals = NavEl[0].lastElementChild.children.length;
+// console.log(`Категория: ${animals}`);
+// console.log(`Количество элементов: ${countAnimals}`);
 
-const technologys = NavEl[2].firstElementChild.textContent;
-const countTechnologys = NavEl[2].lastElementChild.children.length;
-console.log(`Категория: ${technologys}`);
-console.log(`Количество элементов: ${countTechnologys}`);
+// const prodcts = NavEl[1].firstElementChild.textContent;
+// const countProdcts = NavEl[1].lastElementChild.children.length;
+// console.log(`Категория: ${prodcts}`);
+// console.log(`Количество элементов: ${countProdcts}`);
+
+// const technologys = NavEl[2].firstElementChild.textContent;
+// const countTechnologys = NavEl[2].lastElementChild.children.length;
+// console.log(`Категория: ${technologys}`);
+// console.log(`Количество элементов: ${countTechnologys}`);
--- js/task-02.js
+++ js/task-02.js
@@ -1,38 +1,41 @@
+"use strict";
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
-
-
-const potato = document.createElement('li');
-potato.textContent = ingredients[0];
-
-
-const mushroom = document.createElement('li');
-mushroom.textContent = ingredients[1];
-
-
-const garlic = document.createElement('li');
-garlic.textContent = ingredients[2];
-
-
-const tomato = document.createElement('li');
-tomato.textContent = ingredients[3];
-
-
-const greens = document.createElement('li');
-greens.textContent = ingredients[4];
-
-
-const condiments = document.createElement('li');
-condiments.textContent = ingredients[5];
-
 
 const ingredientsList = document.querySelector("#ingredients");
 
+const createItems = (element) => {
+  return ingredients.map((item) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = item;
+    return liEl;
+  });
+};
 
-ingredientsList.append(potato, mushroom, garlic, tomato, greens, condiments);
+const itemsEl = createItems(ingredients);
+
+ingredientsList.append(...itemsEl);
+
+// const potato = document.createElement("li");
+// potato.textContent = ingredients[0];
+
+// const mushroom = document.createElement("li");
+// mushroom.textContent = ingredients[1];
+
+// const garlic = document.createElement("li");
+// garlic.textContent = ingredients[2];
+
+// const tomato = document.createElement("li");
+// tomato.textContent = ingredients[3];
+
+// const greens = document.createElement("li");
+// greens.textContent = ingredients[4];
+
+// const condiments = document.createElement("li");
+// condiments.textContent = ingredients[5];
