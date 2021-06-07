cc318f85008c7434fd32aadba3192560fbc218a1
Anatolii-med <67787169+Anatolii-med@users.noreply.github.com>
2021-06-07 16:24:14 +0300

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -8,18 +8,3 @@ for (const item of itemsEl) {
   console.log(`Категория: ${categoryEl}`);
   console.log(`Количество элементов: ${elCount}`);
 }
-
-// const animals = NavEl[0].firstElementChild.textContent;
-// const countAnimals = NavEl[0].lastElementChild.children.length;
-// console.log(`Категория: ${animals}`);
-// console.log(`Количество элементов: ${countAnimals}`);
-
-// const prodcts = NavEl[1].firstElementChild.textContent;
-// const countProdcts = NavEl[1].lastElementChild.children.length;
-// console.log(`Категория: ${prodcts}`);
-// console.log(`Количество элементов: ${countProdcts}`);
-
-// const technologys = NavEl[2].firstElementChild.textContent;
-// const countTechnologys = NavEl[2].lastElementChild.children.length;
-// console.log(`Категория: ${technologys}`);
-// console.log(`Количество элементов: ${countTechnologys}`);
--- js/task-02.js
+++ js/task-02.js
@@ -10,7 +10,7 @@ const ingredients = [
 
 const ingredientsList = document.querySelector("#ingredients");
 
-const createItems = (element) => {
+const createItems = () => {
   return ingredients.map((item) => {
     const liEl = document.createElement("li");
     liEl.textContent = item;
@@ -21,21 +21,3 @@ const createItems = (element) => {
 const itemsEl = createItems(ingredients);
 
 ingredientsList.append(...itemsEl);
-
-// const potato = document.createElement("li");
-// potato.textContent = ingredients[0];
-
-// const mushroom = document.createElement("li");
-// mushroom.textContent = ingredients[1];
-
-// const garlic = document.createElement("li");
-// garlic.textContent = ingredients[2];
-
-// const tomato = document.createElement("li");
-// tomato.textContent = ingredients[3];
-
-// const greens = document.createElement("li");
-// greens.textContent = ingredients[4];
-
-// const condiments = document.createElement("li");
-// condiments.textContent = ingredients[5];
