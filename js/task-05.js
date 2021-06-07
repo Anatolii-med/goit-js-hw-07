6dbb72f40a73593df466f3388f1ab81b2b0f8310
Anatolii-med <67787169+Anatolii-med@users.noreply.github.com>
2021-06-07 15:31:15 +0300

    5 done

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,9 @@
 const inputEl = document.querySelector("#name-input");
 const nameEl = document.querySelector("#name-output");
 
-inputEl.addEventListener("input", inputValue);
-
-function inputValue(e) {
-  if (e.textContent === "") {
-    return (nameEl.textContent = "незнакомец");
-  } else {
-    return (nameEl.textContent = e.currentTarget.value);
-  }
-}
+inputEl.addEventListener(
+  "input",
+  (event) =>
+    (nameEl.textContent =
+      inputEl.value === "" ? "незнакомец" : event.currentTarget.value)
+);
