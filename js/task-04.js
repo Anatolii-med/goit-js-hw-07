d5bf9c5843b1bb63b3ec28c75916de1a05fa50ff
Anatolii-med <67787169+Anatolii-med@users.noreply.github.com>
2021-06-07 10:18:14 +0300

    task 4 done

--- js/task-04.js
+++ js/task-04.js
@@ -2,17 +2,17 @@ const decrement = document.querySelector('[data-action="decrement"]');
 const increment = document.querySelector('[data-action="increment"]');
 const value = document.querySelector("#value");
 
-let result = 0;
+let result = Number(value.textContent);
 console.log(result);
 
-const add = (value) => {
-  result += 1;
-  value.innerHTML = result;
+const add = () => {
+  result += Number(1);
+  return (value.textContent = result);
 };
-const rem = (value) => {
-  result -= 1;
-  value.textContent = result;
+const rem = () => {
+  result -= Number(1);
+  return (value.textContent = result);
 };
 
-const addBtn = increment.addEventListener("click", add);
-const removeBtn = decrement.addEventListener("click", rem);
+increment.addEventListener("click", add);
+decrement.addEventListener("click", rem);
